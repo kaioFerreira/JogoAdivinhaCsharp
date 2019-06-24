e79978b72613137a0c1a9feaeebbf15c5fefd15f
kaioFerreira <kaiofhs@hotmail.com>
2019-06-24 01:09:01 -0300

    Upando Game para o GitHub

---
+++ Adivinha4/Adivinha4/MainActivity.cs
@@ -0,0 +1,91 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System;
+using AlertDialog = Android.App.AlertDialog;
+
+namespace Adivinha4
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        TextView text_tentativas;
+        TextView text_sorteado;
+        EditText edit_valor;
+        Button cmd_adivinhar;
+
+        //valor inteiro
+        int valor_sorteado;
+        int numero_tentativas;
+
+        //==============================================================
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.MainActivity);
+
+
+            text_tentativas = FindViewById<TextView>(Resource.Id.text_tentativas);
+            text_sorteado = FindViewById<TextView>(Resource.Id.text_sorteado);
+            edit_valor = FindViewById<EditText>(Resource.Id.edit_valor);
+            cmd_adivinhar = FindViewById<Button>(Resource.Id.cmd_adivinhar);
+
+            //iniciar o jogo
+            //sortear um valor
+            Random rnd = new Random();
+            valor_sorteado = rnd.Next(0, 1000);
+            text_sorteado.Text = valor_sorteado.ToString();
+
+            //iniciar tentativas
+            numero_tentativas = 0;
+
+
+            //evento click do cmd_click
+
+            cmd_adivinhar.Click += Cmd_adivinhar_Click;
+
+        }
+
+        //==============================================================
+        private void Cmd_adivinhar_Click(object sender, EventArgs e)
+        {
+            //analisa os dados inseridos versus o valor sorteado
+
+            if (edit_valor.Text == "") return;
+
+            string mensagem;
+            int valor_inserido = int.Parse(edit_valor.Text);
+            if(valor_inserido < valor_sorteado)
+            {
+                mensagem = "O valor inserido é inferior ao sorteado.";
+                numero_tentativas++;
+            }
+            else if(valor_inserido > valor_sorteado)
+            {
+                mensagem = "O valor inserido é superior ao sorteado.";
+                numero_tentativas++;
+            }
+            else
+            {
+                //acertou
+
+                mensagem = "Acertou.";
+
+            }
+            text_tentativas.Text = "Tentativas: " + numero_tentativas.ToString();
+
+            AlertDialog.Builder caixa = new AlertDialog.Builder(this);
+            caixa.SetTitle("Adivinha o Número");
+            caixa.SetMessage(mensagem);
+            caixa.SetPositiveButton("OK", delegate { });
+            caixa.SetCancelable(false);
+            caixa.Show();
+
+        }
+    }
+}
---
+++ Adivinha4/Adivinha4/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -0,0 +1,14 @@
+package mono.android.app;
+
+public class XamarinAndroidEnvironmentVariables
+{
+	// Variables are specified the in "name", "value" pairs
+	public static final String[] Variables = new String[] {
+		"MONO_LOG_LEVEL", "info",
+		"XAMARIN_BUILD_ID", "e62def95-6eb2-4a19-8e36-c436c3b2c472",
+		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
+		"XA_TLS_PROVIDER", "btls",
+		"MONO_GC_PARAMS", "major=marksweep-conc",
+
+	};
+}
